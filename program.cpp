36678d1bb8458a70b856de6e2b5e3d653b173045
RMCPDEDT024 <RMCPDEDT024@roots.com>
2025-07-25 09:36:54 +0530

    code updated

--- program.cpp
+++ program.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " madhan ";
+void printMessage(const vector<string>& msg, const string& separator) {
+    for (size_t i = 0; i < msg.size(); ++i) {
+        cout << msg[i];
+        if (i != msg.size() - 1) cout << separator;
     }
     cout << endl;
+}
+
+vector<string> split(const string& str) {
+    vector<string> result;
+    istringstream iss(str);
+    string word;
+    while (iss >> word) {
+        result.push_back(word);
+    }
+    return result;
+}
+
+int main() {
+    cout << "Enter your message (words separated by spaces): ";
+    string input;
+    getline(cin, input);
+
+    vector<string> msg = split(input);
+
+    cout << "Enter a separator: ";
+    string separator;
+    getline(cin, separator);
+
+    cout << "Your formatted message:\n";
+    printMessage(msg, separator);
+
+    return 0;
 }
